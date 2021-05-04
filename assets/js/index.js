2b523bf63de2cbde08168f95fde571bc83cb01b5
Gabbasov98 <74816663+Gabbasov98@users.noreply.github.com>
2021-05-04 22:20:43 +0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -72,3 +72,14 @@ function openBurgerMenu() {
     $(".header__burger").toggleClass("active");
     $(".nav ul").slideToggle();
 }
+
+$("body").on("click", "a", function(event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
